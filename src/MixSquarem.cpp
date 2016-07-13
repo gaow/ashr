f45cf87238d12129836a8bda13637fb4fa884ae8
Gao Wang <gaow@uchicago.edu>
2016-07-13 16:36:24 -0500

    Kushal update

--- src/MixSquarem.cpp
+++ src/MixSquarem.cpp
@@ -1,54 +1,98 @@
-#include <Rcpp.h>
+#include <cmath>
+#include <thread>
+#include <typeinfo>
 #include <omp.h>
+#include <chrono>
 #include <vector>
+#include <stdlib.h> 
+#include <cmath> //not sure this is necessary
 #include <algorithm>
 
 // [[Rcpp::plugins(openmp)]]
 
 // For Rcpp+OpenMP parallelization, check: https://wbnicholson.wordpress.com/2014/07/10/parallelization-in-rcpp-via-openmp/
+// For Armadillo Support, check: http://arma.sourceforge.net/docs.html
 
 // LDFLAGS:  -L/usr/local/opt/openblas/lib
 // CPPFLAGS: -I/usr/local/opt/openblas/include
 
 using namespace Rcpp;
+using namespace std;
+
+int f(int i) {
+  std::this_thread::sleep_for (std::chrono::seconds(1));
+  return i;
+}
+
+// [[Rcpp::depends(RcppArmadillo)]]
+// [[Rcpp::depends(RcppEigen)]]
 
 // [[Rcpp::export]]
 
-List fixptfn(NumericVector pi_est,NumericMatrix matrix_lik, NumericVector prior){
+int threadcheck(int i)
+{
+  int n_threads = i;
+  std::cout << n_threads << " threads ..." << std::endl;
+  std::vector<int> M(12);
+#pragma omp parallel for num_threads(n_threads)
+  for (int i=0; i<12; i++)
+    M[i] = f(i);
+  return 0;
+}
+
+
+Rcpp::List fixptfn_kushal(NumericVector pi_est,NumericMatrix matrix_lik, NumericVector prior){
+//  clock_t start = clock();
   int n=matrix_lik.nrow(), k=matrix_lik.ncol();
   NumericVector pi_new(k);
-  for (int i=0;i<k;i++){
-    pi_est[i]=std::max(0.0,pi_est[i]);
-  }
-  pi_est=pi_est/sum(pi_est); //normalize pi
-  
   double loglik,lpriordens=0.0;
   NumericMatrix m(n,k);
-  NumericMatrix classprob(m);
+  //  NumericMatrix classprob(m);
   NumericVector m_rowsum(n);
   //IntegerVector subset(prior);
+//  clock_t end = clock();
+//  double elapsed_time_0 = (end-start)/(double)CLOCKS_PER_SEC ;
+//  std::cout << elapsed_time_0 << std::endl;
+  
+//  clock_t start1 = clock() ;
   for (int i=0;i<k;i++){
+    pi_est[i]=std::max(0.0,pi_est[i]);
     m.column(i)=pi_est[i]*matrix_lik.column(i);
     m_rowsum=m_rowsum+m.column(i);
-  }
-  for (int i=0;i<k;i++){
-    classprob.column(i)=classprob.column(i)/m_rowsum;
-  }
-  //calculating objective value--probability
-  loglik=sum(log(m_rowsum));
-  
-  for (int i=0;i<k;i++){
     if(prior[i]!=1.0){
       lpriordens +=(prior[i]-1.0)*log(pi_est[i]);
     }
-    
   }
+//  clock_t end1 = clock() ;
+//  double elapsed_time_1 = (end1-start1)/(double)CLOCKS_PER_SEC ;
+//  std::cout << elapsed_time_1 << std::endl;
+  
+  //  clock_t start2 = clock() ;
+  //  #pragma omp parallel for num_threads(4)
+  //  for (int i=0;i<k;i++){
+  //    m.column(i)=m.column(i)/m_rowsum;
+  //  }
+  //  clock_t end2 = clock() ;
+  //  double elapsed_time_2 = (end2-start2)/(double)CLOCKS_PER_SEC ;
+  
+ // clock_t start3 = clock() ;
+  //calculating objective value--probability
+  loglik=sum(log(m_rowsum));
+//  clock_t end3 = clock() ;
+//  double elapsed_time_3 = (end3-start3)/(double)CLOCKS_PER_SEC ;
+//  std::cout << elapsed_time_3 << std::endl;
+  
   //generating new pi
+//  clock_t start4 = clock() ;
+ #pragma omp parallel for num_threads(4)
   for (int i=0;i<k;i++){//set any estimates that are less than zero, which can happen with prior<1, to 0
-    pi_new[i]=std::max(0.0,sum(classprob.column(i))+prior[i]-1.0);
+    pi_new[i]=std::max(0.0,sum(m.column(i)/m_rowsum)+prior[i]-1.0);
   }
   
   pi_new=pi_new/sum(pi_new); //normalize pi
+//  clock_t end4 = clock() ;
+//  double elapsed_time_4 = (end4-start4)/(double)CLOCKS_PER_SEC ;
+//  std::cout << elapsed_time_4 << std::endl;
   
   return(List::create(Named("fixedpointvector")=pi_new,
                       Named("objfn")=-loglik-lpriordens));
@@ -56,7 +100,7 @@ List fixptfn(NumericVector pi_est,NumericMatrix matrix_lik, NumericVector prior)
 
 
 
-List squarem1(NumericVector par,NumericMatrix matrix_lik,NumericVector prior,List control){
+List squarem2(NumericVector par,NumericMatrix matrix_lik,NumericVector prior,List control){
   //control variables
   int maxiter=control["maxiter"];
   int method_=control["method"];
@@ -83,9 +127,9 @@ List squarem1(NumericVector par,NumericMatrix matrix_lik,NumericVector prior,Lis
   iter=1;
   p=par;
   
-  try{p1list=fixptfn(p,matrix_lik,prior);feval=1;}
+  try{p1list=fixptfn_kushal(p,matrix_lik,prior);feval=1;}
   catch(...){
-    Rcout<<"Error in fixptfn function evaluation";
+    Rcout<<"Error in fixptfn_kushal function evaluation";
     return 1;
   }
   p=p1list["fixedpointvector"];
@@ -99,9 +143,9 @@ List squarem1(NumericVector par,NumericMatrix matrix_lik,NumericVector prior,Lis
     //Step 1
     extrap = true;
     pcpp=p;
-    try{p1list=fixptfn(p,matrix_lik,prior); feval++;}
+    try{p1list=fixptfn_kushal(p,matrix_lik,prior); feval++;}
     catch(...){
-      Rcout<<"Error in fixptfn function evaluation";
+      Rcout<<"Error in fixptfn_kushal function evaluation";
       return 1;
     }
     p1cpp=p1list["fixedpointvector"];
@@ -113,9 +157,9 @@ List squarem1(NumericVector par,NumericMatrix matrix_lik,NumericVector prior,Lis
     
     
     //Step 2
-    try{p2list=fixptfn(p1cpp,matrix_lik,prior);feval++;}
+    try{p2list=fixptfn_kushal(p1cpp,matrix_lik,prior);feval++;}
     catch(...){
-      Rcout<<"Error in fixptfn function evaluation";
+      Rcout<<"Error in fixptfn_kushal function evaluation";
       return 1;
     }
     p2cpp=p2list["fixedpointvector"];
@@ -150,7 +194,7 @@ List squarem1(NumericVector par,NumericMatrix matrix_lik,NumericVector prior,Lis
     
     //Step 4 stabilization
     if(std::abs(alpha-1)>0.01){
-      try{p3list=fixptfn(pnew,matrix_lik,prior);feval++;}
+      try{p3list=fixptfn_kushal(pnew,matrix_lik,prior);feval++;}
       catch(...){
         pnew=p2cpp;
         lnew=p2list["objfn"];
@@ -230,12 +274,9 @@ List cxxMixSquarem(NumericMatrix matrix_lik, NumericVector prior, NumericVector 
     std::fill(pi.begin(), pi.end(), 1./(double)k);
   else{
     pi=clone(pi_init);
-    for (int i=0;i<k;i++)//set any estimates that are very small to be very small
-      pi[i]=std::max(0.0, pi[i]);
-    pi=pi/sum(pi); //normalize pi
   }
   
-  res=squarem1(pi,matrix_lik,prior,control);
+  res=squarem2(pi,matrix_lik,prior,control);
   pi=res["par"];
   loglik=res["value.objfn"];
   niter=res["iter"];
@@ -256,7 +297,7 @@ control.default=list(K = 1, method=3, square=TRUE,
                      step.min0=1, step.max0=1, mstep=4, kr=1, 
                      objfn.inc=1,tol=1.e-07, maxiter=10000, trace=FALSE)
 system.time(for(m in 1:1){
-  ll4 <- cxxMixSquarem(matrix_lik, prior, pi_init=pi_est, control=control.default)
+  ll4 <- cxxMixSquarem2(matrix_lik, prior, pi_init=pi_est, control=control.default)
 })
-  
+
 */
